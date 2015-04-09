6a39c2f1a17911a5af222cfd5cce0dc98957355d
Marco Barrios <mabarriosmarco@gmail.com>
2015-04-08 20:40:37 -0600

    Cambio a Vistas basadas en Clases

--- apprest/views.py
+++ apprest/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import HttpResponse
+from rest_framework.views import APIView
+from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
@@ -13,17 +14,13 @@ from apprest.serializers import ContactoSerializer, DetalleListaContactoSerializ
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def contactos_list(request):
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == 'GET':
+class contactos_list(APIView):
+    def get(self, request, format=None):
         contacto = Contacto.objects.all()
         serializer = ContactoSerializer(contacto, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = ContactoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -31,211 +28,227 @@ def contactos_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
-def listaContactos_list(request):
-    if request.method == 'GET':
+class listaContactos_list(APIView):
+    def get(self, request, format=None):
         lista = ListaContacto.objects.all()
-        serializaer = ListaContactoSerializer(lista, many=True)
-        return Response.serialize(serializaer.data)
+        serializer = ListaContactoSerializer(lista, many=True)
+        return Response.serialize(serializer.data)
 
-    elif request.method == 'POST':
-        serializaer = ListaContactoSerializer(data=request.data)
-        if serializaer.is_valid():
-            serializaer.save()
-            return Response(serializaer.data, status=status.HTTP_201_CREATED)
-        return Response(serializaer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, format=None):
+        serializer = ListaContactoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
-def Lista_detail_list(request):
-    if request.method == 'GET':
+class Lista_detail_list(APIView):
+    def get(self, request, format=None):
         lista = DetalleListaContacto.object.all()
         serializer = DetalleListaContactoSerializer(lista, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = DetalleListaContactoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response.serialize(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
-def tipo_telefono_list(request):
-    if request.method == 'GET':
+class tipo_telefono_list(APIView):
+    def get(self, request, format=None):
         tipos = TipoTelefono.objects.all()
         serializer = TipoTelefonoSerializer(tipos, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = TipoTelefonoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.errors, status=status.HTTP_201_CREATED)
-        return Response.serialize(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
-def telefono_list(request):
-    if request.method == 'GET':
+class telefono_list(APIView):
+    def get(self, request, format=None):
         telefono = Telefono.objects.all()
         serializer = TelefonoSerializer(telefono, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = TelefonoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.errors, status=status.HTTP_201_CREATED)
-        return Response.serialize(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'POST'])
-def evento_list(request):
-    if request.method == 'GET':
+class evento_list(APIView):
+    def get(self, request, format=None):
         telefono = Evento.objects.all()
         serializer = EventoSerializer(telefono, many=True)
         return Response(serializer.data)
 
-    elif request.metheveod == 'POST':
+    def post(self, request, format=None):
         serializer = EventoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.errors, status=status.HTTP_201_CREATED)
-        return Response.serialize(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def lista_contacto_detail(request, pk):
-    try:
-        contactoD = ListaContacto.objects.get(pk=pk)
-    except ListaContacto.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class lista_contacto_detail(APIView):
+    def get_object(self, pk):
+        try:
+            return ListaContacto.objects.get(pk=pk)
+        except ListaContacto.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        contactoD = self.get_object(pk)
         serializer = ListaContactoSerializer(contactoD)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        contactoD = self.get_object(pk)
         serializer = ListaContactoSerializer(contactoD, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        contactoD = self.get_object(pk)
         contactoD.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def contacto_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        contacto = Contacto.objects.get(pk=pk)
-    except Contacto.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class contacto_detail(APIView):
+    def get_object(self, pk):
+        try:
+            return Contacto.objects.get(pk=pk)
+        except Contacto.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        contacto = self.get_object(pk=pk)
         serializer = ContactoSerializer(contacto)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        contacto = self.get_object(pk=pk)
         serializer = ContactoSerializer(contacto, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        contacto = self.get_object(pk=pk)
         contacto.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def detallesLista_detail(request, pk):
+class detallesLista_detail(APIView):
+    def get_obect(self, pk):
+        try:
+            return DetalleListaContacto.objects.get(pk=pk)
+        except DetalleListaContacto.DoesNotExist:
+            raise Http404
 
-    try:
-        detalle = DetalleListaContacto.objects.get(pk=pk)
-    except DetalleListaContacto.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        detalle = self.get_obect(pk=pk)
         serializer = DetalleListaContactoSerializer(detalle)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        detalle = self.get_obect(pk=pk)
         serializer = DetalleListaContactoSerializer(detalle, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        detalle = self.get_obect(pk=pk)
         detalle.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def tipoTelefono_details(request, pk):
-    try:
-        tipo = TipoTelefono.objects.get(pk = pk)
-    except TipoTelefono.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class tipoTelefono_details(APIView):
+    def get_object(self, pk):
+        try:
+            return TipoTelefono.objects.get(pk = pk)
+        except TipoTelefono.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        tipo = self.get_object(pk=pk)
         serializer = TipoTelefonoSerializer(tipo)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk, format=None):
+        tipo = self.get_object(pk=pk)
         serializer = TipoTelefonoSerializer(tipo, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        tipo = self.get_object(pk=pk)
         tipo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def Telefono_details(request, pk):
-    try:
-        telefono = Telefono.objects.get(pk=pk)
-    except Telefono.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class Telefono_details(APIView):
+    def get_object(self, pk):
+        try:
+            return Telefono.objects.get(pk=pk)
+        except Telefono.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        telefono = self.get_object(pk=pk)
         serializer = TelefonoSerializer(telefono)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk, format=None):
+        telefono = self.get_object(pk=pk)
         serializer = TelefonoSerializer(telefono, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        telefono = self.get_object(pk=pk)
         telefono.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def Evento_details(request, pk):
-    try:
-        evento = Evento.objects.get(pk=pk)
-    except Evento.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class Evento_details(APIView):
+    def get_object(self, pk):
+        try:
+            return Evento.objects.get(pk=pk)
+        except Evento.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        evento = self.get_object(pk=pk)
         serializer = EventoSerializer(evento)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, pk, format=None):
+        evento = self.get_object(pk=pk)
         serializer = TelefonoSerializer(evento, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        evento = self.get_object(pk=pk)
         evento.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
