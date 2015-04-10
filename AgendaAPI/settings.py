7c8a200de8812a731706a538ab293ea3050475d4
Enrique Matul <ematul.92@gmail.com>
2015-04-09 23:37:17 -0600

    Modificación de inicio de sesion

--- AgendaAPI/settings.py
+++ AgendaAPI/settings.py
@@ -48,10 +48,14 @@ REST_FRAMEWORK = {
     'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        #'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
 }
 
 
